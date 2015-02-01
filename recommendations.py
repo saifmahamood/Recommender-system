12bd911501f920502866f67fe60029cdd9819122
Saif <saifmahamood@gmail.com>
2015-01-31 19:57:18 -0500

    Creating dataset through delicious API

--- recommendations.py
+++ recommendations.py
@@ -39,6 +39,40 @@ critics = {
 		}
 
 from math import sqrt
+from pydelicious import get_popular, get_userposts, get_urlposts
+
+#initializing user dictionary for getting data
+
+def initializeUserDict(tag ,count = 5):
+	user_dict = {}
+	#get the top count popular posts
+	for p1 in get_popular(tag = tag)[0:count]:
+		for p2 in get_urlposts(p1['url']):
+			user = p2['user']
+			user_dict[user] = {}
+	return user_dict
+
+def fillItems(user_dict):
+	all_items = {}
+	#find links posted by all users
+	for user in user_dict:
+		for i in range(4):
+			try:
+				posts = get_userposts(user)
+				break
+			except:
+				print "" + user + " User Failed, retrying"
+			time.sleep(4)
+		for post in posts:
+			url = post['url']
+			user_dict[user][url] = 1.0
+			all_items[url] = 1
+		# fill missing items with null
+		for ratings in user_dict.values():
+			for item in all_items:
+				if item not in ratings:
+					ratings[item] = 0.0
+
 # Returns a distance-based similarity score for person1 and person2
 def sim_distance(prefs,person1,person2):
 # Get the list of shared_items
